eb3e39c6ef5d624bb675c7a038d03ae69d03651a
jeetee <github@jeetee.net>
2024-12-08 21:25:22 +0100

    D7P2 x

--- 07-main.cpp
+++ 07-main.cpp
@@ -33,6 +33,7 @@ int main()
 }
 
 static bool evaluate_terms(unsigned long long target, unsigned long long seed, vector<unsigned long long>::const_iterator term, vector<unsigned long long>::const_iterator end);
+static bool evaluate_terms_2(unsigned long long target, unsigned long long seed, vector<unsigned long long>::const_iterator term, vector<unsigned long long>::const_iterator end);
 
 static void logic(string fileName)
 {
@@ -40,6 +41,7 @@ static void logic(string fileName)
     std::ifstream input(fileName);
 
     unsigned long long calibration_result { 0 };
+    unsigned long long calibration_result_2 { 0 };
     for (string line; std::getline(input, line);) {
         cout << "." << std::flush; // Progress report
         std::stringstream ss(line);
@@ -52,10 +54,16 @@ static void logic(string fileName)
         if (evaluate_terms(possible_calibration_result, 0, std::cbegin(terms), std::cend(terms)))
         {// Real calibration result
             calibration_result += possible_calibration_result;
+            calibration_result_2 += possible_calibration_result;
+        }
+        else if (evaluate_terms_2(possible_calibration_result, 0, std::cbegin(terms), std::cend(terms)))
+        {
+            calibration_result_2 += possible_calibration_result;
         }
     }
 
-    cout << "\nTotal calibration result = " << calibration_result << endl;
+    cout << "\nTotal calibration result = " << calibration_result
+         << "\nTotal calibration result with concatenation = " << calibration_result_2 << endl;
 }
 
 // PART 1
@@ -88,6 +96,56 @@ static bool evaluate_terms(unsigned long long target, unsigned long long seed, v
     else if (temp_term < target)
     {
         return (evaluate_terms(target, temp_term, std::next(term), end));
+    }
+    // else (temp_term > target): all other code paths have returned already
+    return false;
+}
+
+// PART 2 - also include concatenation as operator
+static bool evaluate_terms_2(unsigned long long target, unsigned long long seed, vector<unsigned long long>::const_iterator term, vector<unsigned long long>::const_iterator end)
+{
+    if (term == end)
+    {
+        return false;
+    }
+
+    // Try multiplication first
+    unsigned long long temp_term = seed * *term;
+    if (temp_term == target)
+    {
+        return true;
+    }
+    else if (temp_term < target)
+    { // Continue trying
+        if (evaluate_terms_2(target, temp_term, std::next(term), end)) {
+            return true;
+        }
+    }
+    // else if > target --> too far, try next option instead
+    // Next option = concatenation
+    std::stringstream concat;
+    concat << seed << *term;
+    concat >> temp_term;
+    if (temp_term == target)
+    {
+        return true;
+    }
+    else if (temp_term < target)
+    { // Continue trying
+        if (evaluate_terms_2(target, temp_term, std::next(term), end)) {
+            return true;
+        }
+    }
+    // else if > target --> too far, try next option instead
+    // Next option = addition
+    temp_term = seed + *term;
+    if (temp_term == target)
+    {
+        return true;
+    }
+    else if (temp_term < target)
+    {
+        return (evaluate_terms_2(target, temp_term, std::next(term), end));
     }
     // else (temp_term > target): all other code paths have returned already
     return false;
